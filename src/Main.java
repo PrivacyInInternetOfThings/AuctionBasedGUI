f9caa27f5cb43b38099f1747c3479d736f1ef29b
merdogan10 <mustafaerdogan1994@gmail.com>
2017-03-06 16:23:50 +0300

    initial dialog

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.util.*;
+import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Runnable r = new Runnable() {
 
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				JPanel gui = new JPanel(new BorderLayout(3, 3));
+
+				
+				
+				JOptionPane.showMessageDialog(null, gui, "Auction Based Traffic Simulation", JOptionPane.PLAIN_MESSAGE);
+
+			}
+			
+		};
+		SwingUtilities.invokeLater(r);
 	}
 
 }
