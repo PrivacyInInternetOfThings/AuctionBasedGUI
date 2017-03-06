ff0c30d5b05ba012a2c0433c559e13eef110449c
merdogan10 <mustafaerdogan1994@gmail.com>
2017-03-06 18:30:36 +0300

    some modifications

--- src/Main.java
+++ src/Main.java
@@ -19,56 +19,101 @@ public class Main {
 				JPanel gui = new JPanel(new BorderLayout(3, 3));
 				gui.setPreferredSize(new Dimension(1200, 400));
 				
+				
 				//Panel For properties of vehicle
-				JPanel propertiesPanel = new JPanel();
+				JPanel general = new JPanel();
+				JPanel labelPanel = new JPanel();
+				JPanel vehicleTypePanel = new JPanel();
+				JPanel emergencyTypePanel = new JPanel();
+				JPanel malfunctionTypePanel = new JPanel();
+				JPanel numOfPeoplePanel = new JPanel();
+				JPanel thresholdPanel = new JPanel();
+				JPanel buttonPanel = new JPanel();
 				
+				general.setLayout(new BoxLayout(general, BoxLayout.X_AXIS));
+				labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.Y_AXIS));
+				vehicleTypePanel.setLayout(new BoxLayout(vehicleTypePanel, BoxLayout.Y_AXIS));
+				emergencyTypePanel.setLayout(new BoxLayout(emergencyTypePanel, BoxLayout.Y_AXIS));
+				malfunctionTypePanel.setLayout(new BoxLayout(malfunctionTypePanel, BoxLayout.Y_AXIS));
+				numOfPeoplePanel.setLayout(new BoxLayout(numOfPeoplePanel, BoxLayout.Y_AXIS));
+				thresholdPanel.setLayout(new BoxLayout(thresholdPanel, BoxLayout.Y_AXIS));
+				buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+				
+				JLabel tableLabel = new JLabel("Parameters: ");
 				JLabel propertiesLabel = new JLabel("Properties: ");
-				JComboBox vehicleTypeChoice = new JComboBox(VEHICLETYPE.values());
-				JComboBox emergencyTypeChoice = new JComboBox(EMERGENCYTYPE.values());
-				JComboBox malfunctionTypeChoice = new JComboBox(MALFUNCTIONTYPE.values());
-				JSpinner numOfPeopleSpinner = new JSpinner(new SpinnerNumberModel(1, 0, 50, 1));
-				JButton randomAssignButton = new JButton("Random Properties");
-				
-				propertiesPanel.add(propertiesLabel);
-				propertiesPanel.add(vehicleTypeChoice);
-				propertiesPanel.add(emergencyTypeChoice);
-				propertiesPanel.add(malfunctionTypeChoice);
-				propertiesPanel.add(numOfPeopleSpinner);
-				propertiesPanel.add(randomAssignButton);
-				
-				//Panel for privacy Values
-				JPanel privacyValuesPanel = new JPanel();
-				
 				JLabel privacyLabel = new JLabel("Privacy Values: ");
+				labelPanel.add(tableLabel,BorderLayout.NORTH);
+				labelPanel.add(propertiesLabel);
+				labelPanel.add(privacyLabel);
 				
-				NumberFormat privacyFormatter = NumberFormat.getNumberInstance();
-				privacyFormatter.setMaximumIntegerDigits(1);
-				privacyFormatter.setMinimumFractionDigits(4);
 				
+				JLabel vehicleTypeLabel = new JLabel("Vehicle Type");
+				JComboBox vehicleTypeChoice = new JComboBox(VEHICLETYPE.values());
 				JSpinner vehicleTypePrivacyField = new JSpinner(new SpinnerNumberModel(0.001,0,1,0.001));
 				((JSpinner.DefaultEditor)vehicleTypePrivacyField.getEditor()).getTextField().setColumns(3);
+				vehicleTypePanel.add(vehicleTypeLabel,BorderLayout.NORTH);
+				vehicleTypePanel.add(vehicleTypeChoice);
+				vehicleTypePanel.add(vehicleTypePrivacyField);
+				
+				
+				JLabel emergencyTypeLabel = new JLabel("Emergency Type");
+				JComboBox emergencyTypeChoice = new JComboBox(EMERGENCYTYPE.values());
 				JSpinner emergencyTypePrivacyField = new JSpinner(new SpinnerNumberModel(0.001,0,1,0.001));
 				((JSpinner.DefaultEditor)emergencyTypePrivacyField.getEditor()).getTextField().setColumns(3);
+				emergencyTypePanel.add(emergencyTypeLabel,BorderLayout.NORTH);
+				emergencyTypePanel.add(emergencyTypeChoice);
+				emergencyTypePanel.add(emergencyTypePrivacyField);
+				
+				
+				JLabel malfunctionTypeLabel = new JLabel("Malfunction Type");
+				JComboBox malfunctionTypeChoice = new JComboBox(MALFUNCTIONTYPE.values());
 				JSpinner malfunctionTypePrivacyField = new JSpinner(new SpinnerNumberModel(0.001,0,1,0.001));
 				((JSpinner.DefaultEditor)malfunctionTypePrivacyField.getEditor()).getTextField().setColumns(3);
+				malfunctionTypePanel.add(malfunctionTypeLabel,BorderLayout.NORTH);
+				malfunctionTypePanel.add(malfunctionTypeChoice);
+				malfunctionTypePanel.add(malfunctionTypePrivacyField);
+				
+				
+				JLabel numOfPeopleLabel = new JLabel("Number of People");
+				JSpinner numOfPeopleSpinner = new JSpinner(new SpinnerNumberModel(1, 0, 50, 1));
 				JSpinner numOfPeoplePrivacyField = new JSpinner(new SpinnerNumberModel(0.001,0,1,0.001));
 				((JSpinner.DefaultEditor)numOfPeoplePrivacyField.getEditor()).getTextField().setColumns(3);
+				numOfPeoplePanel.add(numOfPeopleLabel,BorderLayout.NORTH);
+				numOfPeoplePanel.add(numOfPeopleSpinner);
+				numOfPeoplePanel.add(numOfPeoplePrivacyField);
+				
+				
+				JLabel thresholdLabel = new JLabel("Threshold");
+				JLabel thresholdEmptyLabel = new JLabel(" ");
 				JSpinner thresholdPrivacyField = new JSpinner(new SpinnerNumberModel(0.001,0,1,0.001));
 				((JSpinner.DefaultEditor)thresholdPrivacyField.getEditor()).getTextField().setColumns(3);
-				JButton randomPrivacyValuesButton  = new JButton("Random Privacy Values");
+				thresholdEmptyLabel.setPreferredSize(thresholdPrivacyField.getPreferredSize());
+				thresholdPanel.add(thresholdLabel,BorderLayout.NORTH);
+				thresholdPanel.add(thresholdEmptyLabel);
+				thresholdPanel.add(thresholdPrivacyField);
 				
-				privacyValuesPanel.add(privacyLabel);
-				privacyValuesPanel.add(vehicleTypePrivacyField);
-				privacyValuesPanel.add(emergencyTypePrivacyField);
-				privacyValuesPanel.add(malfunctionTypePrivacyField);
-				privacyValuesPanel.add(numOfPeoplePrivacyField);
-				privacyValuesPanel.add(thresholdPrivacyField);
-				privacyValuesPanel.add(randomPrivacyValuesButton);
+				
+				
+				JButton randomAssignButton = new JButton("Random Properties");
+				JButton randomPrivacyValuesButton  = new JButton("Random Privacy Values");
+				buttonPanel.add(randomAssignButton,BorderLayout.WEST);
+				buttonPanel.add(randomPrivacyValuesButton);
+				
+
+
 				
 				
 				//Show in the main panel
-				gui.add(propertiesPanel, BorderLayout.NORTH);
-				gui.add(privacyValuesPanel);
+				general.add(labelPanel, BorderLayout.WEST);
+				general.add(vehicleTypePanel);
+				general.add(emergencyTypePanel);
+				general.add(malfunctionTypePanel);
+				general.add(numOfPeoplePanel);
+				general.add(thresholdPanel);
+				
+				
+				gui.add(general, BorderLayout.NORTH);
+				gui.add(buttonPanel);
 				JOptionPane.showMessageDialog(null, gui, "Auction Based Traffic Simulation", JOptionPane.PLAIN_MESSAGE);
 
 			}
